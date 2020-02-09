d4d4be0c99339744d6a582272e72d2d60872974e
Marise St-Pierre <naranjaqc@hotmail.com>
2020-02-09 11:34:53 -0500

    input and submit fucntion

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return (
         <div className='ImageLinkForm'>
             <p className='f3'>{'This Magic Brain will detect faces in your pictures. Give it a try.'}</p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='input f4 pa2 w-70 center br2' type='text'/>
-                    <button className='button br2 w-30 grow f4 link ph3 pv2 dib white'>Detect</button>
+                    <input className='input f4 pa2 w-70 center br2' type='text' onChange={onInputChange}/>
+                    <button 
+                    className='button br2 w-30 grow f4 link ph3 pv2 dib white'
+                    onClick={onButtonSubmit}
+                    >Detect</button>
                 </div>
             </div>
         </div>
