da1ceb9cc9044a2c3efc34aac086e7696db934fa
Marise St-Pierre <naranjaqc@hotmail.com>
2020-03-10 17:49:19 -0400

    add routing

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './Navigation.css';
 
-const Navigation = () => {
-    return (
-        <nav className='Navigation'>
-            <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-        </nav>
-    );
+const Navigation = ({onRouteChange, isSignedIn}) => {
+    if (isSignedIn) {
+        return (
+            <nav className='Navigation'>
+                <p onClick={ () => onRouteChange('SignOut')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+            </nav>
+        )
+    } else {
+        return (
+            <nav className='Navigation'>
+                <p onClick={ () => onRouteChange('SignIn')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+                <p onClick={ () => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+            </nav>
+        ) 
+    }
 }
 
 export default Navigation;
